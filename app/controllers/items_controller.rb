0a120489d34f8323e74afc04b1bc1e7c1a5b295b
suey0128 <syui0128@gmail.com>
2021-07-20 20:05:45 -0700

    commit tree 0844282a000f61e8ff044d1f63425c64d4b8ffd8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,46 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    items = Item.all
+    # items = Item.all
+    # render json: items, include: :user
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    new_item = user.items.create!(item_params)
+    render json: new_item, include: :user, status: :created
+  end
+
+  private
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def render_not_found_response
+    render json:{error: "User not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    # render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show] do 
+    resources :items, only: [:index, :show, :create]
+  end
+
+  resources :items, only: [:index] 
 end
